fc2cd5de910f22c358c557fef35a06d4cab40df1
MarvinTerry <marvinterry2004@gmail.com>
2025-12-24 19:26:44 +0800

    fix spi.cpp bug

--- boards/hkust/nxt-dual/src/spi.cpp
+++ boards/hkust/nxt-dual/src/spi.cpp
@@ -44,7 +44,7 @@ constexpr px4_spi_bus_t px4_spi_buses[SPI_BUS_MAX_BUS_ITEMS] = {
 	initSPIBus(SPI::Bus::SPI2, {
 		initSPIDevice(SPIDEV_FLASH(0), SPI::CS{GPIO::PortD, GPIO::Pin4})
 	}),
-	initSPIBus(SPI::Bus::SPI3, {
+	initSPIBusExternal(SPI::Bus::SPI3, {
 		initSPIConfigExternal(SPI::CS{GPIO::PortC, GPIO::Pin0}),
 		initSPIConfigExternal(SPI::CS{GPIO::PortC, GPIO::Pin1}),
 		initSPIConfigExternal(SPI::CS{GPIO::PortA, GPIO::Pin4}),
